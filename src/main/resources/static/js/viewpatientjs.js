5d69e43c990286401f0aae65920db47cbcbff4b4
Mark Fox <markfox47@hotmail.com>
2022-06-21 23:07:37 -0500

    fixed the enable edit button to enable specific fields

--- src/main/resources/static/js/viewpatientjs.js
+++ src/main/resources/static/js/viewpatientjs.js
@@ -1,40 +1,11 @@
-console.log("js file reached");
-const allInputs = document.querySelectorAll("input");
-const editBtn = document.querySelector("#toggleBtn");
-
 $("#toggleBtn").click(function(event) {
     event.preventDefault();
     $.enableEdit();
-    // $("input").toggle();
-    // $("#patientUpdateBtn").toggle();
-})
-
-// $("#patientDeleteBtn").click(function(event) {
-//     // event.preventDefault();
-//     console.log("delete button pressed");
-// })
+});
 
 $.enableEdit = function() {
-
-    // if (editBtn.value === "true") {
-    //     allInputs.readOnly = false;
-    //     editBtn.value = false;
-    // } else {
-    //     allInputs.readOnly = true;
-    //     editBtn.value = true;
-    // }
-
-     // document.getElementsByTagName("input");
-    // console.log(document.querySelector("#firstNameInput").readOnly);
-    // console.log($("#patientForm input").readOnly);
-    // const x = $("#firstNameInput");
-    // if (x.readOnly == true) {
-    //     x.readOnly = false;
-    // } else {
-    //     x.readOnly = false;
-    // }
-    $("#patientUpdateBtn").toggle();
-    $("select").toggle();
+    $("#patientFormFieldset").prop("disabled", false);
+    $("#patientUpdateBtn").prop("disabled", false);
+    $("#patientDeleteBtn").prop("disabled", false);
+    $("#toggleBtn").prop("disabled", true);
 }
-
-// $(document).ready($.enableEdit());
