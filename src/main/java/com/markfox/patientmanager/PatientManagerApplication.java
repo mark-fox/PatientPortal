0ad8b9ce60ecc95a342089f554236ae1db3084e6
Mark Fox <markfox47@hotmail.com>
2022-06-21 21:20:17 -0500

    attempted to refactor project name

--- src/main/java/com/markfox/patientmanager/PatientManagerApplication.java
+++ src/main/java/com/markfox/patientmanager/PatientManagerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PatientmanagerApplication {
+public class PatientManagerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PatientmanagerApplication.class, args);
+		SpringApplication.run(PatientManagerApplication.class, args);
 	}
 
 }
