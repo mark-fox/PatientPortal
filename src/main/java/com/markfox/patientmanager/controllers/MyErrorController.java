b832a4d3428f99ebbd9962671225888a964216b8
Mark Fox <markfox47@hotmail.com>
2022-06-24 15:56:47 -0500

    set up custom error page

---
+++ src/main/java/com/markfox/patientmanager/controllers/MyErrorController.java
@@ -0,0 +1,16 @@
+package com.markfox.patientmanager.controllers;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyErrorController implements ErrorController {
+
+//    Logger logger = LoggerFactory.getLogger(MyErrorController.class);
+
+    @RequestMapping("/error")
+    public String handleError() {
+        return "error";
+    }
+}
