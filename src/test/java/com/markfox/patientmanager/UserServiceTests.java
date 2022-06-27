b9767a53ef56ddeb7d415e4839f41c17da7a3ba4
Mark Fox <markfox47@hotmail.com>
2022-06-27 00:57:51 -0500

    added User and Visit Notes service classes tests

---
+++ src/test/java/com/markfox/patientmanager/UserServiceTests.java
@@ -0,0 +1,24 @@
+package com.markfox.patientmanager;
+
+import com.markfox.patientmanager.models.User;
+import com.markfox.patientmanager.services.UserService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserServiceTests {
+    @Autowired
+    UserService userService;
+
+    @Test
+    public void addNewUserTest() {
+        User user = new User();
+        user.setEmail("test@test.com");
+        user.setPassword("password");
+
+        User savedUser = userService.addNewUser(user);
+        Assertions.assertEquals(savedUser.getEmail(), user.getEmail());
+    }
+}
---
+++ src/test/java/com/markfox/patientmanager/VisitNotesServiceTests.java
@@ -0,0 +1,49 @@
+package com.markfox.patientmanager;
+
+import com.markfox.patientmanager.models.Doctor;
+import com.markfox.patientmanager.models.Patient;
+import com.markfox.patientmanager.models.VisitNotes;
+import com.markfox.patientmanager.services.DoctorService;
+import com.markfox.patientmanager.services.PatientService;
+import com.markfox.patientmanager.services.VisitNotesService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+
+@SpringBootTest
+public class VisitNotesServiceTests {
+    @Autowired
+    VisitNotesService visitNotesService;
+    @Autowired
+    PatientService patientService;
+    @Autowired
+    DoctorService doctorService;
+
+    @Test
+    public void addVisitNotesTest() {
+        Doctor doctor = new Doctor();
+        doctor.setFirstName("Bob");
+        doctor.setLastName("Barker");
+        Doctor savedDoctor = doctorService.addDoctor(doctor);
+
+        Patient patient = new Patient();
+        patient.setFirstName("Matt");
+        patient.setLastName("Damon");
+        patient.setDateOfBirth(LocalDate.now().minusYears(1));
+        patient.setEmailAddress("test@test.com");
+        patient.setDoc(savedDoctor);
+        patientService.addPatient(patient);
+
+        VisitNotes notes = new VisitNotes();
+        notes.setVisitReason("reason");
+        notes.setVisitDate(LocalDate.of(2001,1,1));
+        notes.setDescription("description");
+        notes.setVisitsPatient(patient);
+
+        VisitNotes savedUser = visitNotesService.addVisitNotes(notes);
+        Assertions.assertEquals(savedUser.getVisitReason(), notes.getVisitReason());
+    }
+}
