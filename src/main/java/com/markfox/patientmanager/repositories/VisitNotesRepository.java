6f2ec4f4217b6a037c7d21d34900921d333650f0
Mark Fox <markfox47@hotmail.com>
2022-06-27 13:32:16 -0500

    added custom query for finding Visit Notes and created test for it

--- src/main/java/com/markfox/patientmanager/repositories/VisitNotesRepository.java
+++ src/main/java/com/markfox/patientmanager/repositories/VisitNotesRepository.java
@@ -2,9 +2,16 @@ package com.markfox.patientmanager.repositories;
 
 import com.markfox.patientmanager.models.VisitNotes;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 // Visit Notes Entity Repository
 @Repository
 public interface VisitNotesRepository extends JpaRepository<VisitNotes, Long> {
+    // Custom query to retrieve Visit Notes by ID
+    @Query("FROM VisitNotes v WHERE v.id = :notesId")
+    Optional<VisitNotes> getVisitNotesById(@Param("notesId") Long id);
 }
--- src/test/java/com/markfox/patientmanager/VisitNotesRepositoryTests.java
+++ src/test/java/com/markfox/patientmanager/VisitNotesRepositoryTests.java
@@ -3,6 +3,7 @@ package com.markfox.patientmanager;
 import com.markfox.patientmanager.models.Doctor;
 import com.markfox.patientmanager.models.Patient;
 import com.markfox.patientmanager.models.VisitNotes;
+import com.markfox.patientmanager.repositories.DoctorRepository;
 import com.markfox.patientmanager.repositories.PatientRepository;
 import com.markfox.patientmanager.repositories.VisitNotesRepository;
 import org.junit.jupiter.api.Assertions;
@@ -12,6 +13,7 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -20,6 +22,8 @@ public class VisitNotesRepositoryTests {
     private VisitNotesRepository visitNotesRepository;
     @Autowired
     private PatientRepository patientRepository;
+    @Autowired
+    private DoctorRepository doctorRepository;
 
     // Testing to save Visit Notes to database with Repository
     @Test
@@ -34,6 +38,7 @@ public class VisitNotesRepositoryTests {
         patient.setFirstName("Matt");
         patient.setLastName("Damon");
         patient.setDateOfBirth(LocalDate.now().minusYears(1));
+        patient.setEmailAddress("test@test.com");
         patient.setDoc(new Doctor());
         Patient savedPatient = patientRepository.save(patient);
 
@@ -59,6 +64,7 @@ public class VisitNotesRepositoryTests {
         patient.setFirstName("Matt");
         patient.setLastName("Damon");
         patient.setDateOfBirth(LocalDate.now().minusYears(1));
+        patient.setEmailAddress("test@test.com");
         patient.setDoc(new Doctor());
         Patient savedPatient = patientRepository.save(patient);
 
@@ -71,5 +77,38 @@ public class VisitNotesRepositoryTests {
         Long id = savedNote.getId();
         visitNotesRepository.deleteById(id);
         Assertions.assertNotEquals(id, visitNotesRepository.findById(id));
+    }
+
+    // Testing custom query to find an individual Visit Notes entity by its ID
+    @Test
+    public void getVisitNotesByIdTest() {
+        VisitNotes note = new VisitNotes();
+        String reason = "testing reason";
+        String description = "testing description";
+        LocalDate visitDate = LocalDate.now();
+
+        Doctor doctor = new Doctor();
+        doctor.setFirstName("Bob");
+        doctor.setLastName("Barker");
+        Doctor savedDoctor = doctorRepository.save(doctor);
+
+        // Patient Entity needed to attach Visit Notes to
+        Patient patient = new Patient();
+        patient.setFirstName("Matt");
+        patient.setLastName("Damon");
+        patient.setDateOfBirth(LocalDate.now().minusYears(1));
+        patient.setEmailAddress("test@test.com");
+        patient.setDoc(savedDoctor);
+        Patient savedPatient = patientRepository.save(patient);
+
+        note.setVisitReason(reason);
+        note.setDescription(description);
+        note.setVisitDate(visitDate);
+        note.setVisitsPatient(savedPatient);
+        VisitNotes savedNote = visitNotesRepository.save(note);
+
+        Optional<VisitNotes> foundNote = visitNotesRepository.getVisitNotesById(savedNote.getId());
+        // Tests if the query result is the same as was saved to the database
+        Assertions.assertEquals(foundNote.isPresent() ? foundNote.get().getVisitReason() : "", savedNote.getVisitReason());
     }
 }
